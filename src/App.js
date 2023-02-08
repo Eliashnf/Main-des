00f4cca313e45ece2b2387d32daecd2edf2b50c7
Eliashnf <eliashnf@gmail.com>
2023-02-08 02:37:00 -0800

    Adding 3 pages + adding Navbar + Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer';
+import { NavBar } from './components/Navbar';
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+import Features from './pages/Features';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        
+        <NavBar/>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/Features" element={<Features/>}/>
+            <Route path="/Contact" element={<Contact/>}/>
+        </Routes>
+        <Footer/>
+        
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,28 @@
+import styled from 'styled-components';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+
 export const NavBar = () => {
 
     return(
-    {/*Do it in bootstrap*/}
+        <NavBarContainer>
+        <Navbar bg="light" variant="light">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/Features">Features</Nav.Link>
+              <Nav.Link as={Link} to="/Contact">Contact</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </NavBarContainer>
     )
 }
+
+const NavBarContainer = styled.nav`
+
+
+`;
